55fe2e89638633b51ec0654e31949f6c4505cc17
Robert Nix <bobnixindy@gmail.com>
2024-04-13 14:52:22 -0400

    Update BingoBoard.py
    Add automatically called random numbers, command line arguments via argparse, and other minor updates, change to version 2.0.

--- BingoBoard.py
+++ BingoBoard.py
@@ -5,15 +5,20 @@ from PyQt5.QtWidgets import ( QApplication,
     QLabel,
     QLineEdit,
     QPushButton,
+    QSlider,
     QVBoxLayout,
     QHBoxLayout,
     QMainWindow )
 
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer
 
 from PyQt5.QtGui import QFont
 
 import datetime
+
+import argparse
+
+import bingogame # local to project
 
 class BingoBoard (QMainWindow):
     def __init__(self):
@@ -76,7 +81,113 @@ class BingoBoard (QMainWindow):
         self.show()
 
 
-class Interactive_Window (QMainWindow):
+class Automatic_Window (QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        window_layout = QVBoxLayout()
+        layout = QHBoxLayout()
+        prompt = QLabel("Enter game title: ")
+        layout.addWidget(prompt)
+        self.game_title_entry = QLineEdit()
+        self.game_title_entry.setMaxLength(20)
+        layout.addWidget(self.game_title_entry)
+        self.game_title_commit = QPushButton("Set Title")
+        layout.addWidget(self.game_title_commit)
+        window_layout.addLayout(layout)
+
+        layout = QHBoxLayout()
+        prompt = QLabel("manual | ")
+        layout.addWidget(prompt)
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setMinimum(0)
+        self.slider.setMaximum(60)
+        self.slider.setSingleStep(5)
+        layout.addWidget(self.slider)
+        prompt = QLabel(" | 60 sec -> ")
+        layout.addWidget(prompt)
+        self.timing = QLabel("manual")
+        width = self.timing.sizeHint().width()
+        self.timing.setFixedWidth(width)
+        self.timing.setAlignment(Qt.AlignHCenter)
+        layout.addWidget(self.timing)
+        self.call_time = 0
+        window_layout.addLayout(layout)
+
+        layout = QHBoxLayout()
+        self.pause = QPushButton("Start")
+        layout.addWidget(self.pause)
+        self.paused = True
+        self.clear = QPushButton("Clear")
+        layout.addWidget(self.clear)
+        self.exit_program = QPushButton("Exit")
+        layout.addWidget(self.exit_program)
+        window_layout.addLayout(layout)
+        center = QWidget()
+        center.setLayout(window_layout)
+        self.setCentralWidget(center)
+
+        self.call_timer = QTimer()
+        self.call_timer.timeout.connect(self.call_timer_pop)
+
+        self.game_title_entry.returnPressed.connect(self.commit_game_title)
+        self.game_title_commit.clicked.connect(self.commit_game_title)
+        self.slider.sliderMoved.connect(self.slider_position)
+        self.slider.valueChanged.connect(self.slider_position)
+        self.pause.clicked.connect(self.pause_toggle)
+        self.clear.clicked.connect(self.clear_board)
+        self.exit_program.clicked.connect(self.done)
+
+        self.current_game = bingogame.BingoGame()
+
+        self.show()
+        self.pause.setFocus()
+
+    def commit_game_title(self):
+        call_value = self.game_title_entry.text()
+        self.game_title_entry.setText("")
+        window.game_title.setText(call_value)
+
+    def slider_position(self, t):
+        self.call_time = t
+        self.timing.setText("manual" if t == 0 else str(t))
+
+    def call_timer_pop(self):
+        call_value = self.current_game.next()
+        window.value_labels[call_value].setStyleSheet("color: black; background: white; border: 2px solid")
+        window.current_call.setText(self.current_game.ball_name(call_value))
+
+    def pause_toggle(self):
+        if self.paused:
+            if self.call_time == 0:  # if manual
+                self.call_timer_pop()
+            else:
+                self.pause.setText("Pause")
+                self.call_timer_pop()
+                self.call_timer.start(self.call_time * 1_000)  # time is seconds * 1
+                self.paused = False
+        else:  #pause clicked
+            self.call_timer.stop()
+            self.pause.setText("Start")
+            self.paused = True
+
+    def clear_board(self):
+        self.call_timer.stop()  # end current game
+        self.current_game.game_log("game.record")
+        self.current_game = bingogame.BingoGame()
+
+        # reset the board
+        for i in range(1, 76):
+            window.value_labels[int(i)].setStyleSheet("border: 2px solid")
+            window.current_call.setText("")
+
+    def done(self):
+        if len(self.current_game.called_list()):
+            self.current_game.game_log("game.record")
+        exit(0)
+
+
+class Manual_Window (QMainWindow):
     calls = {}
     called_numbers = list()
     called_numbers.append(datetime.datetime.now().strftime("%m/%d/%Y-%H:%M"))
@@ -171,21 +282,53 @@ class Interactive_Window (QMainWindow):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        screen_title_text = sys.argv[1]
-    else:
-        screen_title_text = "Knights of Columbus 3660"
-    game_title_text = "Bingo Night"
+    parser = argparse.ArgumentParser(prog="BingobBoard",
+        description="""Fullscreen (TV) display of a Bingo call board, along
+        with a control panel for either manually called games or games called
+        by automation in the program.""",
+        epilog="To run, type ./BingoBoard.py")
+
+    parser.add_argument('-s', '--screentitle',
+        default="Knights of Columbus 3660",
+        action="store",
+        dest="screen_title_text",
+        metavar='"top left title"',
+        help="Permanent title displayed on the upper left of the screen. Remains constant during program run.")
+
+    parser.add_argument('-t', '--title',
+        default="Bingo Night",
+        action="store",
+        dest="game_title_text",
+        metavar='"top right message"',
+        help="Variable title displayed in the upper right of the screen. Can be changed via control pane.")
+
+    parser.add_argument('-a', '--automatic',
+        action="store_true",
+        dest="automatic",
+        help="Controls manual or automatic game.")
+
+    parser.add_argument('-v', '--version',
+        action="version", version="%(prog)s 2.0")
+
+    args = parser.parse_args()
+
+    automatic = args.automatic
+    screen_title_text = args.screen_title_text
+    game_title_text = args.game_title_text
 
     app = QApplication([])
 
     window = BingoBoard()
 
-    interactive = Interactive_Window()
-    sg = app.desktop().screenGeometry(interactive)
-    wingeo = interactive.geometry()
-    x = sg.width() - wingeo.width() - 30
-    y = sg.height() - wingeo.height() - 100
-    interactive.move(x, y)
+    if automatic:
+        interface = Automatic_Window()
+    else:
+        interface = Manual_Window()
 
-    exit(app.exec())
+    scr_geo = app.desktop().screenGeometry(interface)
+    win_geo = interface.geometry()
+    x = scr_geo.width() - win_geo.width() - 30
+    y = scr_geo.height() - win_geo.height() - 100
+    interface.move(x, y)
+
+    exit(app.exec_())
